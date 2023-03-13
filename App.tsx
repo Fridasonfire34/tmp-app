bc806595423964e39118a96a5d1fc86e4a05e30e
eduardo.leal <eduardo.leal.or@gmail.com>
2023-03-12 18:00:32 -0600

    feature: refactor initial route and added keychan to verify user id

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Text} from 'react-native';
 
 import {store} from '@app/redux/store';
 import Home from '@app/screens/Home';
@@ -6,16 +7,37 @@ import SignIn from '@app/screens/SignIn';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NativeBaseProvider} from 'native-base';
+import * as Keychain from 'react-native-keychain';
 import {Provider} from 'react-redux';
 
 export default function App() {
+  const [initialRoute, setInitialRoute] = useState<string | null>(null);
+
   const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    const veryfiKey = async () => {
+      const credentials = await Keychain.getGenericPassword();
+      console.log('credentials', credentials);
+      if (credentials) {
+        setInitialRoute('Home');
+      } else {
+        await Keychain.resetGenericPassword();
+        setInitialRoute('SignIn');
+      }
+    };
+    veryfiKey();
+  }, []);
+
+  if (!initialRoute) {
+    return <Text>Loading...</Text>;
+  }
 
   return (
     <Provider store={store}>
       <NativeBaseProvider>
         <NavigationContainer>
-          <Stack.Navigator>
+          <Stack.Navigator initialRouteName={initialRoute as string}>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="SignIn" component={SignIn} />
           </Stack.Navigator>
