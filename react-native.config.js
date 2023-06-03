d6890e0c3222583e460b5bb730d37b09ce616926
eduardo.leal <eduardo.leal.or@gmail.com>
2023-06-03 13:05:49 -0600

    hotfix: update husky

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,8 @@
-module.exports ={
-    project: {
-        ios:{},
-        android:{}
-    },
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
 
-    assets:['./assets/fonts'],
-}
+  assets: ['./assets/fonts'],
+};
